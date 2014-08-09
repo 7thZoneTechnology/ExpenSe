dfb16dbe91d59b1366043e734e00a16737a3c41d
ringfather <rhc245@gmail.com>
2014-08-09 08:47:54 -0400

    update readme to reflect crud

--- outlayRT/mainapp/helpers.py
+++ outlayRT/mainapp/helpers.py
@@ -1,3 +1,6 @@
+from mainapp.models import Expenses, Macros, UserProfile, Budget
+from django.contrib.auth.models import User
+
 def checkIfExpense(request):
 	for item in request:
 		if item.isdigit():
@@ -9,3 +12,16 @@ def checkIfMacro(request):
 		if len(item) == 1:
 			return item
 	return False
+
+def getRemaining():
+	'''returns amount
+	of budget left this month
+	'''
+	pass
+
+def getAmountType(type, start_date):
+	'''for a type of expense 
+	gets the amount spent on that expense
+	from the start_date
+	'''
+	pass
